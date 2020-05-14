281935fca586c8becf4d83b3ed02fda69f6cb3ac
gowthamvarma <gowthamvarma.pec@gmail.com>
2020-05-14 13:18:38 +0530

    checkin from script

---
+++ template/src/main/java/com/gowthamvarma/template/Template.java
@@ -0,0 +1,12 @@
+package com.gowthamvarma.template;
+
+//blog post :: https://www.gowthamvarma.com/
+//youtube tutorial :: https://www.youtube.com/channel/UCBnVpfyt5tnlSMGteks4xYw
+
+public class Template {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
